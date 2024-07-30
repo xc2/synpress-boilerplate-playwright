57e1ceb77dec0d5d5f9fb4751f04e89794850c4b
Kaffi Y. <gh@109cafe.com>
2024-07-30 12:08:45 +0800

    ci: enable provenance (#1)

--- load-dotenv.ts
+++ load-dotenv.ts
@@ -1,6 +1,11 @@
 import * as DotEnv from "dotenv";
 
-const EnvPathList = [".env.local", ".env.ci", ".env"];
+const EnvPathList = [
+  ".env.local",
+  ".env.ci",
+  process.env.NODE_ENV === "development" && ".env.development",
+  ".env",
+].filter(Boolean) as string[];
 
 for (const path of EnvPathList) {
   DotEnv.config({ path });
